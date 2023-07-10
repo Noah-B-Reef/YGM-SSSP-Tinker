2887ce291f5bb9d1f1dd4bccfca873cb616927f7
Noah Reef <noahr@kruskal-login2.lps.umd.edu>
2023-07-10 14:58:08 -0400

    GRAPHS

--- examples/graph.py
+++ examples/graph.py
@@ -6,10 +6,11 @@ import csv
 import sys
 
 # read in command line arguements
-graph_type = sys.argv[0]
-num_nodes = sys.argv[1]
-seed_type = sys.argv[2]
-
+graph_type = sys.argv[1]
+num_nodes = int(sys.argv[2])
+seed_type = int(sys.argv[3])
+data_path = sys.argv[4]
+soln_path = sys.argv[5]
 
 # generate a random graph with weights
 def graph_gen():
@@ -36,19 +37,24 @@ def graph_gen():
         weight = np.random.randint(1,20)
         results.append((edge[0],edge[1],weight))
         results.append((edge[1], edge[0], weight))
-        
+    
+    G = nx.Graph()
+    graph = results
+    G.add_weighted_edges_from(graph)
+
     # sort list before returning
+    sol = [(k,v) for k,v in nx.shortest_path_length(G,0,weight="weight").items()]
     results.sort(key=lambda x: x[0])
-    return results, nx.shortest_path_length(G,0,weight="weight")
+    return results, sol
 
 # convert graph to csv file
 def to_file(data,solution):
     try:
-        f = open("data.csv", "x")
-        g = open("soln.csv", "x")
+        f = open(data_path, "x")
+        g = open(soln_path, "x")
     except:
-        f = open("data.csv", "w")
-        g = open("data.csv", "w")
+        f = open(data_path, "w")
+        g = open(soln_path, "w")
         
     csv_out = csv.writer(f)
     csv_soln = csv.writer(g)
@@ -62,3 +68,5 @@ def to_file(data,solution):
     for row in solution:
         csv_soln.writerow(row)
 
+graph, solutions = graph_gen()
+to_file(graph, solutions)
