0027f9f6e94581da8db9f429df1e52c6c73125ad
Noah Reef <noahr@kruskal-login2.lps.umd.edu>
2023-07-24 11:41:25 -0400

    update runner.py

--- examples/runner.py
+++ examples/runner.py
@@ -1,7 +1,7 @@
 import os
 import time
 import pandas as pd
-
+import csv
 
 df = pd.DataFrame(columns=["scale", "wall time(ms)", "# of edges"])
 
@@ -20,6 +20,17 @@ for i in range(2,100):
     os.system("srun -o my_graph.csv ../build/examples/rmat_gen_graphs " + str(i))
     print("csv made!")
     time.sleep(1)
+
+
+     # open file
+    with open("my_graph.csv", "r") as csvfile:
+        data=[tuple(line) for line in csv.reader(csvfile)]
+    data = data[1::]
+    data.sort()
+
+    df = pd.DataFrame(data, columns=["source", "end", "weight"])
+    df.to_csv("my_graph.csv")
+
     os.system("srun -o exp.out ../build/examples/sssp my_graph.csv")
     print("sssp ran!")
     time.sleep(1)
